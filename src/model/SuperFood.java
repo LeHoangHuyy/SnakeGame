8d2157d81647085e4066af79209e401c9b378c6a
Kendy205 <thanhtung002zzz@gmail.com>
2025-04-11 14:55:40 +0700

    update class superfood

---
+++ src/model/SuperFood.java
@@ -0,0 +1,10 @@
+package model;
+
+public class SuperFood extends Food {
+
+	public SuperFood(int gameWidth, int gameHeight, int tileSize) {
+		super(gameWidth, gameHeight, tileSize);
+		// TODO Auto-generated constructor stub
+	}
+
+}
